83477de67761d1606c5330c9ab0df3477093a5c1
Philippe Serhal <philippe.serhal@netlify.com>
2024-09-20 14:06:20 -0400

    feat: on-demand tag-based cache revalidation

--- env.d.ts
+++ env.d.ts
@@ -8,13 +8,18 @@ import type {
   HydrogenSessionData,
   HydrogenEnv,
 } from '@shopify/hydrogen';
-import type { createAppLoadContext } from '~/lib/context';
+import type {createAppLoadContext} from '~/lib/context';
 
 declare global {
   /**
    * A global `process` object is only available during build to access NODE_ENV.
    */
-  const process: { env: { NODE_ENV: 'production' | 'development' } };
+  const process: {
+    env: {
+      NODE_ENV: 'production' | 'development';
+      SHOPIFY_WEBHOOK_SECRET?: string;
+    };
+  };
 
   interface Env extends HydrogenEnv {
     // declare additional Env parameter use in the fetch handler and Remix loader context here
